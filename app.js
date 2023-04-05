b4ca36302e01e40828c05482c42cdbc7a55a6844
孙豪鹏 <haopeng@sunhaopengdeMacBook-Pro.local>
2023-04-04 22:16:04 -0700

    a9 Mongo sp22

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ import express from 'express';
 import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UsersController from "./controllers/users/users-controller.js";
-import TuitsController
-    from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect('mongodb://127.0.0.1:27017/tuiter')
 
 const app = express()
 
